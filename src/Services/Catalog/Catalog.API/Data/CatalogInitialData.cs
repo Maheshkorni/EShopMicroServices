b6c18c490a6c1fc950635e81e6cedd2ed5935ec8
MaheshKorni <korninagamahesh@gmail.com>
2026-04-06 11:32:48 +0530

    Section -7 Completed

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,34 @@
+﻿using Marten.Schema;
+
+namespace Catalog.API.Data
+{
+    public class CatalogInitialData : IInitialData
+    {
+        public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+        {
+            using var session = store.LightweightSession();
+
+            if (await session.Query<Product>().AnyAsync())
+                return;
+
+            session.Store<Product>(GetPreconfiguredProduct());
+            await session.SaveChangesAsync();
+         }
+
+        private static IEnumerable<Product> GetPreconfiguredProduct() => new List<Product>() 
+        {
+            new Product
+            {
+                Id = new Guid("26dad760-f610-44da-9560-05a607f4f9ca"),
+                Name = "New Product D",
+                Category = ["c1","c3"],
+                Description = "Description Product D",
+                ImageFile = "ImageFile Product D",
+                Price = 299
+            }
+        };
+    }
+
+    
+    
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -16,7 +16,13 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+if (builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
@@ -25,5 +31,5 @@ var app = builder.Build();
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 
-
+app.UseHealthChecks("/health",new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse});
 app.Run();
